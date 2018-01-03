197188bf9112d0845c8c2e486b508727509f5d32
Hong-Jie Huang <giffea@gmail.com>
2018-01-04 01:43:13 +0800

    test input last ball is availabe with 2 strike pass

--- test/BowlingScoreCalculatorTest.py
+++ test/BowlingScoreCalculatorTest.py
@@ -101,6 +101,16 @@ class BowlingScoreCalculatorTest(unittest.TestCase):
         # Then it is not a valid input
         self.assertFalse(result)
 
+    def testInputLastBallAvailableWith2Strike(self):
+        # Given the pinfall of frame 10 has 2 strike and last pinfal > 0
+        inputs = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 1]
+
+        # When check if the inputs are valid numbers
+        result = self.calculator.checkInputs(inputs)
+
+        # Then it is a valid input
+        self.assertTrue(result)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
