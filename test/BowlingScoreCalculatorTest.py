64ccc41740fd5d796823c4581a1596257c307339
Hong-Jie Huang <giffea@gmail.com>
2018-01-03 23:29:53 +0800

    test pinfall < 0 fail

--- test/BowlingScoreCalculatorTest.py
+++ test/BowlingScoreCalculatorTest.py
@@ -21,6 +21,16 @@ class BowlingScoreCalculatorTest(unittest.TestCase):
         # Then it is a valid input
         self.assertTrue(result)
 
+    def testInputPinfallLessThan0(self):
+        # Given the pinfall which < 0
+        inputs = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1]
+
+        # When check if the inputs are valid numbers
+        result = self.calculator.checkInputs(inputs)
+
+        # Then it is not a valid input
+        self.assertFalse(result)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
